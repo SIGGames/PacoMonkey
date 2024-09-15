2d7669724c509e4f75fc434ac263ba99f2de8916
Aniol0012 <aniol0012@gmail.com>
2024-09-15 12:53:52 +0200

    feat: add fields to globalconfig

--- Assets/Scripts/Configuration/GlobalConfiguration.cs
+++ Assets/Scripts/Configuration/GlobalConfiguration.cs
@@ -8,30 +8,15 @@ namespace Configuration {
         // If any the fields wants to do not be modified in the inspector,
         // use the readonly keyword. In case the field is a constant, use the const keyword.
 
+        [Header("Debug Configuration")]
+        public bool isDebugMode = false;
+        public bool isGodMode = false;
+
         [Header("Screen Configuration")]
         // These fields are hided in the inspector because they are constants
         public const int DefaultScreenWidth = 1920;
         public const int DefaultScreenHeight = 1080;
         public const bool DefaultFullScreen = true;
-
-        [Header("Player Configuration")]
-        public float playerSpeed = 5.0f;
-        public float playerJumpForce = 12.0f;
-        public int playerMaxHealth = 100;
-
-        [Header("Enemy Configuration")]
-        public float enemySpeed = 3.0f;
-        public int enemyMaxHealth = 50;
-
-        [FormerlySerializedAs("DefaultHp")] [Header("Health Configuration")]
-        // TODO: Refactor this to determine player and enemy health
-        public int defaultHp = 100;
-
-        public int maxHp = 100;
-        public int defaultLives = 1;
-        public int maxLives = 10;
-        public const int DefaultHpIncrement = 1;
-        public const int DefaultHpDecrement = 1;
 
         [Header("Physics Configuration")]
         public const float GravityScale = 1.0f;
@@ -41,12 +26,42 @@ namespace Configuration {
         public float musicVolume = 0.8f;
         public float sfxVolume = 0.7f;
 
+        [Header("Player Configuration")]
+        public float playerSpeedRun = 7.0f;
+        public float playerSpeedWalk = 2.33f;
+        public float playerSpeedCrouchWalk = 1.5f;
+        public float playerJumpForce = 12.0f;
+        public float playerSmallJumpForce = 6.0f;
+        public float playerBigJumpForce = 12.0f;
+        public float playerDoubleJumpForce = 6.0f;
+
+        [Header("Player Health Configuration")]
+        public int defaultHp = 100;
+        public int maxHp = 100;
+        public int defaultLives = 1;
+        public int maxLives = 10;
+        public const int DefaultHpIncrement = 1;
+        public const int DefaultHpDecrement = 1;
+
+        [Header("Character Sizes")]
+        public float pacoSmallJumpHeightMultiplier = 1.5f;
+        public float pacoAdultMiccaJumpHeightMultiplier = 2.0f;
+
+        [Header("Enemy Configuration")]
+        public float enemySpeed = 3.0f;
+        public int enemyMaxHealth = 50;
+
+        [Header("Idle Animations")]
+        public float idleExtendedDelay1 = 6.0f;
+        public float idleExtendedDuration1 = 3.0f;
+        public float idleExtendedDelay2 = 18.0f;
+
+        [Header("Crouch Configuration")]
+        public float crouchSlideDuration = 1.5f;
+        public float crouchSlideSpeed = 5.0f;
+
         [Header("Environment Configuration")]
         public float windSpeed = 2.0f;
-
-        [Header("Debug Configuration")]
-        public bool isDebugMode = false;
-        public bool isGodMode = false;
 
         private void Awake() {
             SetGlobalConfigInstance();
