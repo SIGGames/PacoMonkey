60e4686357955403f24e5cd5a7445a08d9e3c709
Aniol0012 <aniol0012@gmail.com>
2024-09-30 00:00:23 +0200

    feat: add quit game btn to ui panel

---
+++ Assets/Scripts/UI/QuitGame.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace UI {
+    public class QuitGame : MonoBehaviour {
+        public void Quit() {
+            Application.Quit();
+
+            #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+            #endif
+        }
+    }
+}
