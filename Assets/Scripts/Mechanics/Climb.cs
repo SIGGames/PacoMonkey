149419545c88326364662c04a7b03ecd3d11fea4
Aniol0012 <aniol0012@gmail.com>
2024-09-29 19:55:32 +0200

    fix[#72]: climb animation

--- Assets/Scripts/Mechanics/Climb.cs
+++ Assets/Scripts/Mechanics/Climb.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using Enums;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Mechanics {
     public class Climb : MonoBehaviour {
@@ -8,26 +6,35 @@ namespace Mechanics {
         private const float Speed = 8f;
         private bool _isLadder;
         private bool _isClimbing;
+        private bool _isHolding;
 
         private Rigidbody2D _rb;
         private PlayerController _playerController;
+        // private Animator _animator;
 
         private void Awake() {
             _rb = GetComponent<Rigidbody2D>();
             _playerController = GetComponent<PlayerController>();
+            // _animator = GetComponent<Animator>();
         }
 
         private void Update() {
             _vertical = Input.GetAxis("Vertical");
 
-            if (_isLadder && Utils.Keybinds.GetClimbKey()) {
-                _isClimbing = true;
+            if (_isLadder) {
+                ShowClimbIndicator(true);
+
+                if (Utils.Keybinds.GetClimbKey() || _isHolding) {
+                    _isClimbing = true;
+                    _isHolding = true;
+                }
             }
-            else if (!_isLadder) {
+            else {
+                ShowClimbIndicator(false);
                 _isClimbing = false;
+                _isHolding = false;
             }
 
-            // TODO: Add animation for climbing, and add reference on Awake method
             // _animator.SetBool("Climbing", _isClimbing);
         }
 
@@ -37,6 +44,10 @@ namespace Mechanics {
                 _rb.velocity = new Vector2(0, _vertical * Speed);
 
                 _playerController.velocity.y = 0f;
+            }
+            else if (_isHolding) {
+                _rb.gravityScale = 0f;
+                _rb.velocity = Vector2.zero;
             }
             else {
                 _rb.gravityScale = 4f;
@@ -52,9 +63,14 @@ namespace Mechanics {
         private void OnTriggerExit2D(Collider2D collision) {
             if (collision.CompareTag("Ladder")) {
                 _isLadder = false;
-                _rb.gravityScale = 4f;
                 _isClimbing = false;
+                _isHolding = false;
+                _rb.gravityScale = 4f;
             }
+        }
+
+        private void ShowClimbIndicator(bool show) {
+            // TODO: Add code to show or hide the climb indicator like enabling/disabling a UI GameObject or a message on screen
         }
     }
 }
