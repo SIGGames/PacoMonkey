8e4ffca0be6c615683fc34d9117f963672aad064
Aniol0012 <aniol0012@gmail.com>
2024-11-29 20:24:33 +0100

    fix: player land animation

--- Assets/Scripts/Gameplay/PlayerLanded.cs
+++ Assets/Scripts/Gameplay/PlayerLanded.cs
@@ -2,10 +2,6 @@ using Mechanics.Movement;
 using Platformer.Core;
 
 namespace Platformer.Gameplay {
-    /// <summary>
-    /// Fired when the player character lands after being airborne.
-    /// </summary>
-    /// <typeparam name="PlayerLanded"></typeparam>
     public class PlayerLanded : Simulation.Event<PlayerLanded> {
         public PlayerController player;
 
