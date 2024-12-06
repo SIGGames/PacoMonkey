bd0842a47f1182a60cbc0d12425e6777db93d689
Aniol0012 <aniol0012@gmail.com>
2024-12-06 22:38:41 +0100

    feat: add character manager

---
+++ Assets/Scripts/Enums/Character.cs
@@ -0,0 +1,7 @@
+﻿namespace Enums {
+    public enum Character {
+        Micca,
+        Micca2,
+        Paco
+    }
+}
---
+++ Assets/Scripts/Managers/CharacterManager.cs
@@ -0,0 +1,65 @@
+﻿using Enums;
+using Mechanics.Movement;
+using UnityEngine;
+
+namespace Managers {
+    public class CharacterManager : MonoBehaviour {
+        [System.Serializable]
+        public class CharacterConfiguration {
+            public Character characterType;
+            public Sprite characterSprite;
+            public Vector2 colliderOffset;
+            public Vector2 colliderSize;
+            public float crouchSpeedMultiplier;
+            public float maxRunSpeed;
+            public float jumpModifier;
+        }
+
+        [Header("Character Configurations")]
+        public CharacterConfiguration[] characters;
+
+        [Header("References")]
+        public SpriteRenderer spriteRenderer;
+        public BoxCollider2D boxCollider;
+        public PlayerController playerController;
+
+        private Character _currentCharacter;
+
+        private void Start() {
+            SetCharacter(Character.Micca);
+        }
+
+        public void SetCharacter(Character character) {
+            foreach (var config in characters) {
+                if (config.characterType == character) {
+                    ApplyCharacterConfiguration(config);
+                    _currentCharacter = character;
+                    return;
+                }
+            }
+
+            Debug.LogWarning($"Character {character} not found in configurations.");
+        }
+
+        private void ApplyCharacterConfiguration(CharacterConfiguration config) {
+            spriteRenderer.sprite = config.characterSprite;
+
+            // Update the collider
+            if (boxCollider != null) {
+                boxCollider.offset = config.colliderOffset;
+                boxCollider.size = config.colliderSize;
+            }
+
+            // Update player settings
+            if (playerController != null) {
+                playerController.crouchSpeedMultiplier = config.crouchSpeedMultiplier;
+                playerController.maxRunSpeed = config.maxRunSpeed;
+                playerController.jumpModifier = config.jumpModifier;
+            }
+        }
+
+        public Character GetCurrentCharacter() {
+            return _currentCharacter;
+        }
+    }
+}
