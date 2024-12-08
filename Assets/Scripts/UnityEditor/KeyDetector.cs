f194f48aba11448ae9019d6061b40e8a16104174
Aniol0012 <aniol0012@gmail.com>
2024-12-08 15:54:23 +0100

    feat: add key detector for debugging

---
+++ Assets/Scripts/UnityEditor/KeyDetector.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace UnityEditor {
+    public class KeyDetector : MonoBehaviour {
+        void Update() {
+            foreach (KeyCode keyCode in System.Enum.GetValues(typeof(KeyCode))) {
+                if (Input.GetKeyDown(keyCode)) {
+                    Debug.Log("Key pressed: " + keyCode);
+                }
+            }
+        }
+    }
+}
