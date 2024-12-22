826c7d4240cfad1e16b1265d089a006b9f6bfb6d
Aniol0012 <aniol0012@gmail.com>
2024-12-22 20:15:16 +0100

    feat: add test enviroment 2 to scene

---
+++ Assets/Scripts/Mechanics/Environment/PlayerDamageTile.cs
@@ -0,0 +1,22 @@
+﻿using Controllers;
+using UnityEditor;
+using UnityEngine;
+
+namespace Mechanics.Environment {
+    public class PlayerDamageTile : MonoBehaviour {
+        [TagSelector]
+        [SerializeField] private string damageTagTile;
+
+        private PlayerController _player;
+
+        private void Awake() {
+            _player = GetComponent<PlayerController>();
+        }
+
+        private void OnTriggerEnter2D(Collider2D other) {
+            if (other.CompareTag(damageTagTile) && other.gameObject.layer == LayerMask.NameToLayer("Ground")) {
+                _player.lives.DecrementLive();
+            }
+        }
+    }
+}
