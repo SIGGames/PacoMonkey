34e3b6594fe644569fb04c5214840a078c0e9dba
Aniol0012 <aniol0012@gmail.com>
2025-02-04 18:11:09 +0100

    refactor: removed unused code

--- Assets/Scripts/Gameplay/PlayerJumped.cs
+++ Assets/Scripts/Gameplay/PlayerJumped.cs
@@ -1,18 +1,14 @@
 using Controllers;
-using Mechanics.Movement;
 using Platformer.Core;
 
-namespace Platformer.Gameplay {
-    /// <summary>
-    /// Fired when the player performs a Jump.
-    /// </summary>
-    /// <typeparam name="PlayerJumped"></typeparam>
+namespace Gameplay {
     public class PlayerJumped : Simulation.Event<PlayerJumped> {
         public PlayerController player;
 
         public override void Execute() {
-            if (player.audioSource && player.jumpAudio)
+            if (player.audioSource && player.jumpAudio) {
                 player.audioSource.PlayOneShot(player.jumpAudio);
+            }
         }
     }
 }
