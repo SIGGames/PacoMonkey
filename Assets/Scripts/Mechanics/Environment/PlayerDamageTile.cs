01352d4e6965cd7d3152dff0a8a48519e7ffd097
Aniol0012 <aniol0012@gmail.com>
2025-02-22 00:25:58 +0100

    feat: improved player damage tile

--- Assets/Scripts/Mechanics/Environment/PlayerDamageTile.cs
+++ Assets/Scripts/Mechanics/Environment/PlayerDamageTile.cs
@@ -1,17 +1,15 @@
 ﻿using Managers;
 using UnityEditor;
 using UnityEngine;
-using static Utils.LayerUtils;
 using static Utils.TagUtils;
 
 namespace Mechanics.Environment {
-
     public class PlayerDamageTile : MonoBehaviour {
         [SerializeField, HalfStepSlider(0, 10)]
         private float damage = 1f;
 
         private void OnTriggerEnter2D(Collider2D col) {
-            if (col.CompareTag(DamageTile) && col.gameObject.layer == Ground) {
+            if (col.CompareTag(DamageTile) || col.CompareTag(Player)) {
                 CharacterManager.Instance.currentPlayerController.lives.DecrementLives(damage);
             }
         }
