acb7272b5ffaff66934bc04b6360860f517f5f49
Aniol0012 <aniol0012@gmail.com>
2025-03-04 22:08:12 +0100

    feat: add character switch zone

---
+++ Assets/Scripts/Zones/CharacterSwitch.cs
@@ -0,0 +1,17 @@
+﻿using Enums;
+using Managers;
+using UnityEngine;
+using static Utils.TagUtils;
+
+namespace Zones {
+    [RequireComponent(typeof(Collider2D))]
+    public class CharacterSwitch : MonoBehaviour {
+        [SerializeField] private Character character = Character.Micca1;
+
+        private void OnTriggerEnter2D(Collider2D col) {
+            if (col.gameObject.CompareTag(Player) && CharacterManager.Instance.GetCurrentCharacter() != character) {
+                CharacterManager.Instance.SetCharacter(character);
+            }
+        }
+    }
+}
