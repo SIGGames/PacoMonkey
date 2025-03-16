095b87e5165005f3936b19fb921640f276dca8eb
Aniol0012 <aniol0012@gmail.com>
2025-03-16 15:06:26 +0100

    feat: add translations alphabetical sorter

---
+++ Assets/Scripts/Tools/SortTranslations.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using Localization;
+using UnityEditor;
+
+namespace Tools {
+    public static class SortTranslations {
+        [MenuItem("Tools/Sort Translations")]
+        public static void SortTranslationsData() {
+            const string assetPath = "Assets/Prefabs/Translations.asset";
+
+            LocalizationData translationsData = AssetDatabase.LoadAssetAtPath<LocalizationData>(assetPath);
+            if (translationsData == null) {
+                return;
+            }
+
+            translationsData.texts = translationsData.texts
+                .OrderBy(t => t.key)
+                .ToList();
+
+            EditorUtility.SetDirty(translationsData);
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+        }
+    }
+}
