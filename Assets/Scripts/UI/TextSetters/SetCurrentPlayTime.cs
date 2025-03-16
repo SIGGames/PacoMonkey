f10e4228d78e291e2dd3640f5c8d8eff6acc6800
Aniol0012 <aniol0012@gmail.com>
2025-03-16 13:37:30 +0100

    feat: improved main menu

---
+++ Assets/Scripts/UI/TextSetters/SetCurrentPlayTime.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using static Utils.PlayerPrefsKeys;
+
+namespace UI.TextSetters {
+    public class SetCurrentPlayTime : MonoBehaviour {
+        [SerializeField] private TextMeshProUGUI textComponent;
+        [SerializeField] private bool getSessionTime;
+
+        private float _playTime;
+        private float _sessionTime;
+
+        private void Start() {
+            if (!getSessionTime) {
+                _playTime = PlayerPrefs.GetFloat(CurrentPlayTimeKey, 0);
+            } else {
+                _sessionTime = 0;
+            }
+
+            UpdatePlayTimeText();
+        }
+
+        private void Update() {
+            float deltaTime = Time.unscaledDeltaTime;
+
+            if (getSessionTime) {
+                _sessionTime += deltaTime;
+            } else {
+                _playTime += deltaTime;
+                PlayerPrefs.SetFloat(CurrentPlayTimeKey, _playTime);
+                PlayerPrefs.Save();
+            }
+
+            UpdatePlayTimeText();
+        }
+
+        private void UpdatePlayTimeText() {
+            TimeSpan timeSpan = TimeSpan.FromSeconds(getSessionTime ? _sessionTime : _playTime);
+            textComponent.text = $"{timeSpan.Hours:D2}:{timeSpan.Minutes:D2}:{timeSpan.Seconds:D2}";
+        }
+    }
+}
---
+++ Assets/Scripts/UI/TextSetters/SetMenuText.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace UI.TextSetters {
+    public class SetMenuText : MonoBehaviour {
+        [SerializeField] private TextMeshProUGUI textComponent;
+        [SerializeField] private TitleTextType textType;
+
+        private string _text;
+
+        private void Start() {
+            _text = textType switch {
+                TitleTextType.GameName => Application.companyName,
+                TitleTextType.CompanyName => Application.productName,
+                TitleTextType.Version => Application.version,
+                _ => throw new ArgumentOutOfRangeException()
+            };
+
+            textComponent.text = _text;
+        }
+    }
+
+    public enum TitleTextType {
+        GameName,
+        CompanyName,
+        Version
+    }
+}
