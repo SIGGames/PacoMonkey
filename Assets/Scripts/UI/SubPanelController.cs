e6121ff06550a9a0e43837f87d58663cd0ee4aa0
Aniol0012 <aniol0012@gmail.com>
2025-03-16 18:27:37 +0100

    feat: add audio UI option

---
+++ Assets/Scripts/UI/SubPanelController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace UI {
+    public class SubPanelController : MonoBehaviour {
+        public GameObject[] subPanels;
+
+        public void SetActiveSubPanel(int index) {
+            for (int i = 0; i < subPanels.Length; i++) {
+                bool active = i == index;
+                if (subPanels[i].activeSelf != active) {
+                    subPanels[i].SetActive(active);
+                }
+            }
+        }
+
+        private void OnEnable() {
+            SetActiveSubPanel(0);
+        }
+    }
+}
