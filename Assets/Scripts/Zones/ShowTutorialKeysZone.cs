a86f48ac055951c4511bb0d58cbf522b85187497
Aniol0012 <aniol0012@gmail.com>
2025-04-16 23:07:31 +0200

    fix: simplified tutorial keys to work properly

--- Assets/Scripts/Zones/ShowTutorialKeysZone.cs
+++ Assets/Scripts/Zones/ShowTutorialKeysZone.cs
@@ -20,8 +20,6 @@ namespace Zones {
         [SerializeField] private GameObject popupPrefab;
         [SerializeField] private GameObject iconPrefab;
 
-        private readonly List<InputAction> _listenedActions = new();
-        private bool _active;
         private bool _activated;
 
         private void OnTriggerEnter2D(Collider2D col) {
@@ -44,12 +42,10 @@ namespace Zones {
             if (_activated && !canReopen) return;
 
             popupPrefab.SetActive(true);
-            _active = true;
             _activated = true;
 
             SetUpTitleText();
             SetupIcons();
-            EnableActionListeners();
         }
 
         private void ClosePopUp() {
@@ -58,9 +54,6 @@ namespace Zones {
             }
 
             popupPrefab.SetActive(false);
-            _active = false;
-
-            DisableActionListeners();
             ClearIcons();
         }
 
@@ -75,17 +68,11 @@ namespace Zones {
             ClearIcons();
 
             foreach (TutorialActionBinding tutorialAction in actionsToShow) {
-                InputAction action = tutorialAction.actionReference.action;
-                if (action == null) {
-                    continue;
-                }
-                _listenedActions.Add(action);
-
                 string controlPath = GetControlPath(tutorialAction);
                 Sprite sprite = PlayerInputManager.Instance.GetInputSprite(controlPath);
                 if (sprite != null) {
                     GameObject iconObj = Instantiate(iconPrefab, keysContainer);
-                    iconObj.name = $"Icon_{action.name}_{controlPath}";
+                    iconObj.name = $"Icon_{titleTextKey}_{controlPath}";
                     iconObj.GetComponentInChildren<Image>().sprite = sprite;
                 }
             }
@@ -101,33 +88,11 @@ namespace Zones {
             foreach (Transform child in keysContainer) {
                 Destroy(child.gameObject);
             }
-
-            _listenedActions.Clear();
-        }
-
-        private void EnableActionListeners() {
-            foreach (var action in _listenedActions) {
-                action.performed += OnAnyActionPerformed;
-            }
-        }
-
-        private void DisableActionListeners() {
-            foreach (var action in _listenedActions) {
-                action.performed -= OnAnyActionPerformed;
-            }
-        }
-
-        private void OnAnyActionPerformed(InputAction.CallbackContext ctx) {
-            if (!_active) return;
-
-            popupPrefab.SetActive(false);
-            DisableActionListeners();
         }
     }
 
     [Serializable]
     public struct TutorialActionBinding {
-        public InputActionReference actionReference;
         public string keyBoardInputActionName;
         public string gamepadInputActionName;
     }
