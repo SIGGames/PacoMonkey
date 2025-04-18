65bf2a610253b120b2b6983f8bfed45b94aaded4
Aniol0012 <aniol0012@gmail.com>
2025-04-18 20:31:19 +0200

    chore: update background image

---
+++ Assets/Scripts/View/Parallax.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+namespace View {
+    public class Parallax : MonoBehaviour {
+        [SerializeField, Range(0f, 1f)] private float lagAmount;
+
+        private Vector3 _previousCameraPosition;
+        private Transform _camera;
+        private Vector3 _targetPosition;
+
+        private float ParallaxAmount => 1f - lagAmount;
+
+        private void Awake() {
+            if (Camera.main != null) {
+                _camera = Camera.main.transform;
+            }
+
+            _previousCameraPosition = _camera.position;
+        }
+
+        private void LateUpdate() {
+            if (_camera == null) {
+                return;
+            }
+
+            // + movement.y * ParallaxAmount
+            Vector3 movement = CameraMovement;
+            if (movement != Vector3.zero) {
+                _targetPosition = new Vector3(transform.position.x + movement.x * ParallaxAmount, transform.position.y,
+                    transform.position.z);
+                transform.position = _targetPosition;
+            }
+        }
+
+        private Vector3 CameraMovement {
+            get {
+                Vector3 movement = _camera.position - _previousCameraPosition;
+                _previousCameraPosition = _camera.position;
+                return movement;
+            }
+        }
+    }
+}
