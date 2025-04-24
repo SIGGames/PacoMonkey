5addc1b0a62e49893bbf1598ea382a5b1552f484
Aniol0012 <aniol0012@gmail.com>
2025-04-24 16:31:39 +0200

    feat: enhance hold v2

--- Assets/Scripts/Mechanics/Movement/Hold_v2.cs
+++ Assets/Scripts/Mechanics/Movement/Hold_v2.cs
@@ -10,15 +10,27 @@ namespace Mechanics.Movement {
         [SerializeField, Range(0, 1)]
         private float onHoldXOffset, onHoldYOffset;
 
+        [Header("On finish climb ledge")]
+        [SerializeField]
+        private Vector2 onFinishClimbLedgeOffset = new(0f, 0f);
+
+        [SerializeField]
+        private Vector2 extraOffsetOnWallClimb = new(0f, 0f);
+
+        [Header("Ledge Detection")]
+        [SerializeField]
+        private bool displayGizmos;
+
         [SerializeField, Range(0, 1)]
         private float redXOffset, redYOffset, redXSize, redYSize, greenXOffset, greenYOffset, greenXSize, greenYSize;
 
-        [SerializeField]
-        private bool displayGizmos;
+        [SerializeField, Range(0, 1)]
+        private float ledgeCeilingCheckHeight = 0.3f;
 
         private Rigidbody2D _rb;
         private float _startingGrav;
         private bool _greenBox, _redBox;
+        private bool _playerWasWallClimbing;
 
         private bool _isGrabbing;
         private Animator _animator;
@@ -42,7 +54,7 @@ namespace Mechanics.Movement {
             _redBox = Physics2D.OverlapBox(new Vector2(transform.position.x + GetXOffset(redXOffset),
                 transform.position.y + redYOffset), new Vector2(redXSize, redYSize), 0f, Ground.value);
 
-            if (_greenBox && !_redBox && !_isGrabbing && !_player.IsGrounded) {
+            if (_greenBox && !_redBox && !_isGrabbing && !_player.IsGrounded && !IsCeilingAbove()) {
                 StartHold();
             }
         }
@@ -53,15 +65,17 @@ namespace Mechanics.Movement {
             }
             _isGrabbing = true;
 
+            if (_player.movementState == PlayerMovementState.WallClimb) {
+                _playerWasWallClimbing = true;
+            }
+
             // To ensure that map is not colliding with player during the ledge climb
             _player.boxCollider.isTrigger = true;
-            _rb.bodyType = RigidbodyType2D.Kinematic;
             _rb.gravityScale = 0f;
             _rb.velocity = Vector2.zero;
-
             _player.AddPosition(GetXOffset(onHoldXOffset), onHoldYOffset);
             _animator.SetBool(IsHolding, true);
-            _player.SetMovementState(PlayerMovementState.Hold, 2);
+            _player.SetMovementState(PlayerMovementState.Hold, 5);
             _player.FreezePosition();
         }
 
@@ -69,20 +83,37 @@ namespace Mechanics.Movement {
             _player.UnlockMovementState();
             _player.SetMovementState(PlayerMovementState.Climb, 2);
 
+            // This is to avoid displaying the falling animation
+            _player.IsGrounded = true;
+
             // This starts the ledge climb animation
             _animator.SetBool(IsHolding, false);
+        }
+
+        private bool IsCeilingAbove() {
+            Vector2 origin = new Vector2(transform.position.x, transform.position.y + _player.boxCollider.bounds.extents.y);
+            RaycastHit2D hit = Physics2D.Raycast(origin, Vector2.up, ledgeCeilingCheckHeight, Ground.value);
+            return hit.collider != null;
         }
 
         public void OnFinishClimbLedge() {
             // Set player grounded since it will be grounded after the ledge climb, this is to prevent the player falling animation
             _player.IsGrounded = true;
             _isGrabbing = false;
-            _rb.bodyType = RigidbodyType2D.Kinematic;
             _rb.gravityScale = _startingGrav;
             _rb.velocity = Vector2.zero;
-            _player.boxCollider.isTrigger = false;
 
+            // When the player was wall climbing, we need to add the extra offset to the player position since the detection
+            // was made a bit earlier and the player is not in on the exact same position
+            if (_playerWasWallClimbing) {
+                _player.AddPosition(GetXOffset(extraOffsetOnWallClimb.x), extraOffsetOnWallClimb.y);
+            } else {
+                _player.AddPosition(GetXOffset(onFinishClimbLedgeOffset.x), onFinishClimbLedgeOffset.y);
+            }
+
+            _playerWasWallClimbing = false;
             _player.UnlockMovementState();
+            _player.boxCollider.isTrigger = false;
             _player.FreezePosition(false);
         }
 
