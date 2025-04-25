575f1c6171d3a54d256e0339cffc5484c4953802
Aniol0012 <aniol0012@gmail.com>
2025-04-26 00:18:39 +0200

    fix: update language translation of current zone

--- Assets/Scripts/UI/TextSetters/SetZoneText.cs
+++ Assets/Scripts/UI/TextSetters/SetZoneText.cs
@@ -1,4 +1,5 @@
-﻿using Controllers;
+﻿using System;
+using Controllers;
 using Enums;
 using Localization;
 using TMPro;
@@ -14,16 +15,28 @@ namespace UI.TextSetters {
             _textComponent = GetComponent<TextMeshProUGUI>();
         }
 
+        private void OnEnable() {
+            SetCurrentMapZone();
+            UpdateZoneText();
+        }
+
         private void Update() {
             if (GameController.Instance.currentMapZone == _previousMapZone) {
                 return;
             }
 
+            SetCurrentMapZone();
+            UpdateZoneText();
+        }
+
+        private void SetCurrentMapZone() {
             MapZone currentMapZone = GameController.Instance.currentMapZone;
             _previousMapZone = currentMapZone;
+        }
 
+        private void UpdateZoneText() {
             // Retrieve the localized text for the current map zone
-            string zoneKey = "mz-" + currentMapZone.ToString().ToLower();
+            string zoneKey = "mz-" + _previousMapZone.ToString().ToLower();
             _textComponent.text = LocalizationManager.Instance.GetLocalizedText(zoneKey);
         }
     }
