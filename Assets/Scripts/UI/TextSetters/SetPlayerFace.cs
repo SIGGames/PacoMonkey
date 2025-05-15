d260f8f7e20cd0ef7df8a41a68689890ac88bf45
Aniol0012 <aniol0012@gmail.com>
2025-05-15 17:55:31 +0200

    fix: revert image preserve aspect

--- Assets/Scripts/UI/TextSetters/SetPlayerFace.cs
+++ Assets/Scripts/UI/TextSetters/SetPlayerFace.cs
@@ -12,9 +12,6 @@ namespace UI.TextSetters {
             }
 
             image.sprite = sprite;
-
-            // Ensure the image do not gets deformed on different resolutions
-            image.preserveAspect = true;
         }
     }
 }
