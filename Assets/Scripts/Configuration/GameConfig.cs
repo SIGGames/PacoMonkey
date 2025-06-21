c50edaa86fb9b8ae04902d4f0f6200c27caba7f7
Aniol0012 <aniol0012@gmail.com>
2025-06-21 13:04:45 +0200

    chore: update game version

--- Assets/Scripts/Configuration/GameConfig.cs
+++ Assets/Scripts/Configuration/GameConfig.cs
@@ -2,7 +2,7 @@
 
 namespace Configuration {
     public static class GameConfig {
-        public const string GameVersion = "v1.0.1";
+        public const string GameVersion = "v1.0.2";
 
         public const Language DefaultLanguage = Language.Catalan;
 
